7c6bcc16fbd9f28755376cf8647caaf569f7f0e9
Krystian Jabłonowski <33730467+Criesto@users.noreply.github.com>
2020-07-23 08:29:24 +0200

    Added main file

---
+++ convert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Choose encoding You want to decode to UTF-8 (FROM encoding)
+source_encoding='iso-8859-1'
+
+# Recurivly find all files in directories and subdirectories
+for file in $(find . -type f -print0 | xargs -0)
+do
+	# check encoding of the file and print only the 'charset'
+	encoding=$(file -i $file | awk '{print $3}')
+	# echo "$encoding"
+	if [[ $encoding == "charset=${source_encoding}" ]]; then
+		( iconv -f $source_encoding -t utf-8 $file -o $file | xargs -0 )
+	fi
+done
